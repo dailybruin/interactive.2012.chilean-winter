9c594db3071115eec4198c056a7f5b979e8d88bc
thecalvinchan <calvin.chan.h@gmail.com>
2013-02-16 19:19:16 -0800

    almost there. Implemented AJAX loading into lightbox for mini profiles. Still need to style stuff and optimize EVERYTHING. Audio does not work on Firefox, because firefox does not support mp3. Will need to convert to OGG. Hopefully, I can do that through Adapter.

--- js/mosaic.js
+++ js/mosaic.js
@@ -2,7 +2,7 @@ $(document).ready(
 function() {
 
 	var mosaicHeight = $('#mosaicContainer').height();
-	$('#mosaicTitle').css({'width':(3*mosaicHeight/4)-10,'height':mosaicHeight/4-10})
+	$('#mosaicTitle').css({'width':(2*mosaicHeight/4)-10,'height':mosaicHeight/4-10})
 
 	//largecontainers
 	$('.largecontainer').each(
@@ -39,7 +39,7 @@ function() {
 		function(){
 			var widthSmall = ($(this).parent().width()/2)-10;
 			//$('.tiles').animate({'width':widthSmall,'height':widthSmall},300,function(){$(this).css('z-index','0');});
-			var width = $(this).width()*1.3;
+			var width = $(this).width()*1.2;
 			$(this).css('z-index','3');
 			var index = $(this).index();
 			switch(index)
@@ -64,36 +64,63 @@ function() {
 		},
 		function(){
 			var width = ($(this).parent().width()/2)-10;
-			$(this).animate({'width': width,'height':width}, 300, function(){$(this).css('z-index','0');});
+			$(this).animate({'width': width,'height':width}, 50, function(){$(this).css('z-index','0');});
 		}
 	);
 
-		$('.largecontainer').each(
-			function(){
-				randomIndex = Math.floor(Math.random()*10/2.5);
-				//console.log('random Index '+ randomIndex);
-				var width = $(this).children().width()*1.3;
-				$(this).children().eq(randomIndex).css({'z-index':'2','width': width,'height':width});
-				var index = $(this).index();
-			switch(randomIndex)
-			{
-				case 0: 
-				$(this).children().eq(randomIndex).css({'border-bottom':'10px solid #fff','border-right':'10px solid #fff'});
-				break;
+	/**$('.largecontainer').each(
+		function(){
+			randomIndex = Math.floor(Math.random()*10/2.5);
+			//console.log('random Index '+ randomIndex);
+			var width = $(this).children().width()*1.2;
+			$(this).children().eq(randomIndex).css({'z-index':'2','width': width,'height':width});
+			var index = $(this).index();
+		switch(randomIndex)
+		{
+			case 0: 
+			$(this).children().eq(randomIndex).css({'border-bottom':'10px solid #fff','border-right':'10px solid #fff'});
+			break;
 
-				case 1: 
-				$(this).children().eq(randomIndex).css({'border-bottom':'10px solid #fff','border-left':'10px solid #fff'});
-				break;
+			case 1: 
+			$(this).children().eq(randomIndex).css({'border-bottom':'10px solid #fff','border-left':'10px solid #fff'});
+			break;
 
-				case 2: 
-				$(this).children().eq(randomIndex).css({'border-top':'10px solid #fff','border-right':'10px solid #fff'});
-				break;
+			case 2: 
+			$(this).children().eq(randomIndex).css({'border-top':'10px solid #fff','border-right':'10px solid #fff'});
+			break;
 
-				case 3: 
-				$(this).children().eq(randomIndex).css({'border-top':'10px solid #fff','border-left':'10px solid #fff'});
-				break;
-			}
-			});
+			case 3: 
+			$(this).children().eq(randomIndex).css({'border-top':'10px solid #fff','border-left':'10px solid #fff'});
+			break;
+		}
+		});**/
+
+	$('.miniProfiles').click(function(e){
+		var name=$(this).attr('name');
+		$('#background').fadeIn(1000);
+		$('#profile').fadeIn(1000);
+		$("#ajax").load("profiles/" + name + ".html", function(){
+		console.log("Clicked " + name);});
+		e.preventDefault();
+	});
+
+	$('#background, .exitupper').click(function(e){
+        $('#profile').fadeOut(700,function(){
+        	$("#ajax").empty();
+        });
+        $('#background').fadeOut(1000);
+        e.preventDefault();
+    });
+
+    $(document).keyup(function(e){
+        if (e.keyCode==27)
+        {
+            $('.vcontent').hide("slide",{direction:"left"},700,function(){
+            $('#backdrop').css({'z-index':5}).fadeOut(700);
+            });
+        }
+    });
+	$("#lightbox").empty();
 
 }
 );
